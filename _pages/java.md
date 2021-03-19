f848ad6058ad2d78e528633ef68fc50110670c34
ChangyeonYoo <devwgod@gmail.com>
2021-03-19 23:55:53 +0900

    태그, 카테고리 설정

---
+++ _pages/java.md
@@ -0,0 +1,9 @@
+---
+title: "java"
+permalink: /categories/java/
+layout: category
+author_profile: true
+taxonomy: java
+---
+
+java에 대해 공부한 내용입니다.
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "github.io 블로그 시작하기",
+        "excerpt":"이 글의 제목은 github.io 블로그 시작하기이고 마지막으로 수정된 시간은 2019-04-13 08:06:00 -0500이다.  ","categories": ["Blog"],
+        "tags": ["Blog"],
+        "url": "/blog/test/",
+        "teaser": null
+      }]
